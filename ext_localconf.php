21f6c748d74acd23300999b00c89b83f1e4c7564
Simon Praetorius <praetorius@wiro-consultants.de>
2014-01-10 16:58:07 +0100

    include dependencies

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,6 +2,12 @@
 
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
+// Load Selenese parser
+include(t3lib_extMgm::extPath('caretaker_selenium') . 'vendor/php-selenese/lib/Selenese/__init__.php');
+
+// Load webdriver library
+include(t3lib_extMgm::extPath('caretaker_selenium') . 'vendor/php-webdriver/lib/__init__.php');
+
 	// load Service Helper
 include_once(t3lib_extMgm::extPath('caretaker').'classes/helpers/class.tx_caretaker_ServiceHelper.php');
 tx_caretaker_ServiceHelper::registerCaretakerService ($_EXTKEY , 'services' , 'tx_caretakerselenium'   ,'Selenium Test', 'Run A Selenium Test' );
