8654d122f1b428aab4df77598a18e0794ece763c
Simon Praetorius <praetorius@wiro-consultants.de>
2014-01-22 13:45:10 +0100

    update selenium test class to use Selenium WebDriver instead of Selenium 1
    * use php-selenese library to convert Selenese commands to valid WebDriver calls
    * use php-webdriver library to connect to Selenium WebDriver instance or Selenium Grid instance to run the tests

--- classes/class.tx_caretakerselenium_SeleniumTest.php
+++ classes/class.tx_caretakerselenium_SeleniumTest.php
@@ -1,8 +1,5 @@
 <?php
 
-require_once 'class.tx_caretakerselenium_Selenium.php';
-require_once 'class.tx_caretakerselenium_SeleniumCommand.php';
-
 /**
  tx_caretakerselenium_SeleniumTest manages the commands and executes them through a tx_caretakerseleneium_Selenium
 
@@ -13,46 +10,64 @@ require_once 'class.tx_caretakerselenium_SeleniumCommand.php';
  */
 
 class tx_caretakerselenium_SeleniumTest {
+	protected $sel;
+	protected $host;
+	protected $connectTimeout;
 
-	private $sel;
-	private $commands = array();
-	private $testSuccessful = true;
-	private $commandsText = '';
-	private $commandsLoaded = false;
-	private $host;
+	protected $browser;
+	protected $browserWidth;
+	protected $browserHeight;
 
-	function __construct($commands,$browser,$baseUrl,$host) {
+	protected $baseURL;
 
-		//echo $commands;
+	protected $commands = array();
+	protected $commandsText = '';
+	protected $commandsLoaded = false;
 
-		$this->commandsText = $commands;
+	protected $testSuccessful = true;
+
+	public function __construct($commands, $browser, $baseUrl, $host, $connectTimeout = 5000, $browserWidth = 1280, $browserHeight = 960) {
+		// Selenium server information
+		$this->host = $host;
+		$this->connectTimeout = $connectTimeout;
 		$this->browser = $browser;
-		$this->baseURL = $baseUrl;
-		$this->host    = $host;
+		$this->browserWidth = $browserWidth;
+		$this->browserHeight = $browserHeight;
+
+		// Selenium commands
+		$this->commandsText = $commands;
+
+		// Ensure that the base url has a trailling slash
+		$this->baseURL = rtrim($baseUrl, '/') . '/';
 	}
 
-	function run(){
-
+	public function run() {
 		$this->setUp();
 		$res = $this->testMyTestCase();
 		$this->tearDown();
+
 		return $res;
 	}
 
-	function setUp()  {
+	protected function setUp()  {
+		// Initialize web driver
+		$capabilities = array(\WebDriverCapabilityType::BROWSER_NAME => $this->browser);
+		$this->sel = \RemoteWebDriver::create($this->host, $capabilities, $this->connectTimeout);
 
-		$this->sel = new tx_caretakerselenium_Selenium($this->browser,$this->baseURL, $this->host);
-		if($this->loadCommands()) {
+		// Set browser window dimensions
+		$this->sel->manage()->window()->setSize(
+			new \WebDriverDimension($this->browserWidth, $this->browserHeight)
+		);
 
+		// Parse commands
+		if ($this->loadCommands()) {
 			$this->commandsLoaded = true;
-			$this->sel->start();
 		}
 	}
 
-	function tearDown() {
-
-		$this->sel->stop();
-
+	protected function tearDown() {
+		// Close connection to selenium session
+		$this->sel->quit();
 	}
 
 	/**
@@ -68,149 +83,191 @@ class tx_caretakerselenium_SeleniumTest {
 	 * if no timer commands are used the time is 0, should be everytime green
 	 *
 	 */
-	function testMyTestCase() {
+	protected function testMyTestCase() {
+		if (!$this->commandsLoaded) {
+			// Commands are not ready yet
+			return array(false);
+		}
 
-		if($this->commandsLoaded) {
-				
-			// $avoidWaitForPageToLoad = false; // needed for ie fix, UPDATE: not needed at the moment
-			$time = 0; // the measured time
-			$starttime = microtime(true); // time is started automatically
-			$lastRound = $starttime;
-			$timerRunning = true; // indicates if the timer is running
-			$timeLogArray = array();
+		// $avoidWaitForPageToLoad = false; // needed for ie fix, UPDATE: not needed at the moment
+		$time = 0; // the measured time
+		$starttime = microtime(true); // time is started automatically
+		$lastRound = $starttime;
+		$timerRunning = true; // indicates if the timer is running
+		$timeLogArray = array();
 
-			foreach($this->commands as $command) {
+		foreach ($this->commands as $command) {
+			// @ indicates a custom command
+			if (isset($command->command) && substr($command->command, 0, 1) === '@') {
 
-				// @ indicates a custom command
-				if(substr($command->command, 0, 1) == '@') {
+				// reset the start timer, because the time should start now
+				// if called when the timer is not running starts to count the time
+				switch ($command->command) {
+					case '@resetTimer':
+						$starttime = microtime(true);
+						$lastRound = $starttime;
+						$timerRunning = true;
+						if (count($timeLogArray)) {
+							$timeLogArray[] = ':resetTimeLog:';
+						}
+						break;
 
-					// reset the start timer, because the time shoold start now
-					// if called when the timer is not running starts to count the time
-					switch($command->command){
-
-						case '@resetTimer':
-
+					case '@startTimer':
+						if (!$timerRunning) {
 							$starttime = microtime(true);
 							$lastRound = $starttime;
 							$timerRunning = true;
-							if (count($timeLogArray)) $timeLogArray[] = ':resetTimeLog:';
-							break;
+						}
+						break;
 
-						case '@startTimer':
-								
-							if(!$timerRunning) {
-								$starttime = microtime(true);
-								$lastRound = $starttime;
-								$timerRunning = true;
-							}
-							break;
-
-						case '@stopTimer':
-
-							if($timerRunning) {
-								$timeLogArray[] = round( microtime(true) - $lastRound,  2).' '.$command->comment ;
-								$lastRound = microtime(true);
-								$time += microtime(true) - $starttime;
-								$timerRunning = false;
-							}
-							break;
-							
-						case '@stopTimer':
-
-							if($timerRunning) {
-								$timeLogArray[] = round( microtime(true) - $lastRound,  2).' '.$command->comment ;
-								$lastRound = microtime(true);
-								$time += microtime(true) - $starttime;
-								$timerRunning = false;
-							}
-							break;
-							
-						case '@pause':
-							$duration = ceil( $command->params[0] / 10000 );
-							if ( $duration > 0 ) {
-								sleep ( $duration );
-							} else {
-								sleep (1);
-							}
-							break;
-							
-					}
+					case '@stopTimer':
+						if ($timerRunning) {
+							$timeLogArray[] = round(microtime(true) - $lastRound, 2) . ' ' . $command->comment;
+							$lastRound = microtime(true);
+							$time += microtime(true) - $starttime;
+							$timerRunning = false;
+						}
+						break;
 						
-					// continue with the next command
-					continue;
+					case '@stopTimer':
+						if ($timerRunning) {
+							$timeLogArray[] = round(microtime(true) - $lastRound, 2) . ' ' . $command->comment;
+							$lastRound = microtime(true);
+							$time += microtime(true) - $starttime;
+							$timerRunning = false;
+						}
+						break;
+						
+					case '@pause':
+						$duration = ceil($command->arg1 / 1000);
+						if ($duration > 0) {
+							sleep($duration);
+						} else {
+							sleep(1);
+						}
+						break;
 				}
 					
-				$message = $this->sel->executeCommand($command);
-				if($message != 'OK') {
-						
-					$this->sel->stop();
-					$this->testSuccessful = false;
-					$msg = 'An error occured: Command:'.$command->command.' at line '.$command->lineInFile.' in your commands. Message: '.$message;
-					if(!empty($command->comment)) {
+				// continue with the next command
+				continue;
+			}
+			
+			// Execute web driver command
+			$result = $command->runWebDriver($this->sel);
 
-						$msg .= ' Comment: '.$command->comment."\n";
-					}
-					return array(false,$msg.' '.implode(':',$timeLogArray),0);
-				}
-			}
-				
-			// if timer is running, now stop it
-			if($timerRunning) {
-				$time += microtime(true) - $starttime;
-			}
-				
-			if($this->testSuccessful) {
-				return array(true, implode(':',$timeLogArray)  , $time);
-			} else {
-				return array(false, implode(':',$timeLogArray)  , $time);
+			// Not successful?
+			if (!$result->success) { // $result->continue
+				// Abort test
+				$this->testSuccessful = false;
+				break;
 			}
 		}
-		return array(false);
+
+		// if timer is running, now stop it
+		if ($timerRunning) {
+			$time += microtime(true) - $starttime;
+		}
+
+		// Generate time log output
+		$msg = implode(':', $timeLogArray);
+
+		if ($this->testSuccessful) {
+			return array(
+				true,
+				$msg,
+				$time
+			);
+		} else {
+			if (isset($result) && isset($command)) {
+				// Generate custom error message
+				$msg = 'An error occured: Command:' . $command->command . ' at line ' . $command->lineInFile . ' in your commands. Message: ' . $result->message;
+				if (!empty($command->comment)) {
+					$msg .= ' Comment: '.$command->comment."\n";
+				}
+			}
+
+			return array(
+				false,
+				$msg,
+				$time
+			);
+		}
 	}
 
-	function loadCommands() {
-		 
-		$commandsLines = explode("\n",$this->commandsText);
-		if(empty($commandsLines)) {
+	protected function loadCommands() {
+		$commandsLines = explode("\n", trim($this->commandsText));
 
-			fwrite(STDERR,'An error occured: No commands were found!'."\n");
+		// No commands available
+		if (empty($commandsLines)) {
+			fwrite(STDERR, "An error occured: No commands were found!\n");
 			return false;
 		}
+
 		$lineNumber = 1;
-		$lastCommand;
-
-		foreach($commandsLines as $command) {
-
+		$lastCommand = $commandText = $paramCount = null;
+		foreach ($commandsLines as $command) {
 			$command = trim($command);
-			$firstChar = substr(trim($command),0,1);
-			if( !empty($command) && ($firstChar != '-' && $firstChar != '#' && $firstChar != ':' ) ) {
-					
-				$commandText = substr($command,0,strpos($command,':'));
-				$commentText = substr($command,strpos($command,':') + 1);
-				$lastCommand = new tx_caretakerselenium_SeleniumCommand($commandText, $lineNumber, $commentText);
-				$paramCount = 1;
+			$firstChar = substr($command, 0, 1);
 
-				$this->commands[] = $lastCommand;
+			switch ($firstChar) {
+				// Add parameters to last command object
+				case '-':
+					if (isset($lastCommand) && $paramCount <= 2) {
+						$varName = "arg$paramCount";
+						$lastCommand->$varName = trim(substr($command, 1));
 
-			} elseif($firstChar == '-') {
-					
-				if(!empty($paramCount) && $paramCount < 2) {
+						switch ($commandText) {
+							// Add full URL to open command
+							case 'open':
+								if ($paramCount === 1) {
+									$lastCommand->$varName = $this->baseURL . ltrim($lastCommand->$varName, '/');
+								}
+								break;
+						}
 
-					if(!$lastCommand->addParam(trim(substr($command,1)))) {
-							
 						$paramCount++;
-						fwrite(STDERR,'There are too many parameters for that command or wrong syntax at line'.$lineNumber.'.'."\n");
-						return false;
 					}
-				}
-			} elseif($firstChar == ':') {
+					break;
 
-				$lastCommand->addComment(trim(substr($command,1)));
+				// Add comments to last command object
+				case ':':
+					if (isset($lastCommand)) {
+						$lastCommand->comment .= ' ' . trim(substr($command, 1));
+					}
+					break;
+
+				// Ignore disabled lines
+				case '#':
+					break;
+
+				// Add new command
+				default:
+					// Strip comments from command name
+					$commandParts = explode(':', $command, 2);
+					$commandText = $commandParts[0];
+
+					// Check if command is already implemented
+					$commandClass = '\\Selenese\\Command\\' . preg_replace('#[^a-zA-Z0-9_]#', '', $commandText);
+					if ($firstChar !== '@' && class_exists($commandClass)) {
+						$lastCommand = new $commandClass();
+					} else {
+						// Not implemented, so use stub class
+						$lastCommand = new \Selenese\Command\Stub();
+						$lastCommand->command = $commandText;
+					}
+
+					// Add debugging information to command object
+					$lastCommand->comment = (isset($commandParts[1])) ? $commandParts[1] : '';
+					$lastCommand->lineInFile = $lineNumber;
+
+					$this->commands[] = $lastCommand;
+					$paramCount = 1;
+					break;
 			}
+
 			$lineNumber++;
 		}
+
 		return true;
 	}
 }
-
-?>
